7d1d54a9ed53de1ebd8db1c02de539385cfb1c37
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2024-09-16 21:32:14 -0400

    si

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const target = process.env.BACKEND;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: target,
         changeOrigin: true,
       },
     },
