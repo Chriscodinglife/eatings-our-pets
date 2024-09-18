07d7068416d80e164482fe93a1776d4c5b08d7fa
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2024-09-18 19:57:25 -0400

    dotenv

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
-const target = process.env.BACKEND;
-
+const target = process.env.VITE_FRONTEND;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
