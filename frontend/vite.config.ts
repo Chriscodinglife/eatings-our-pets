5b9b0401dc43e569dbd7be23d23db5729697ce6e
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2024-09-18 20:31:43 -0400

    yes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-dotenv.config();
 
-const target = process.env.VITE_BACKEND;
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: target,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const target = env.VITE_BACKEND;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: target,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
