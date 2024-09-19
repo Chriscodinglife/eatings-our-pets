ff7e7a1d2f18425219b38ccf30872895248f3754
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2024-09-18 21:00:13 -0400

    trying cors stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import http from "https";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACK_END,
+          changeOrigin: true,
+          secure: false,
+          agent: new http.Agent(),
+        },
+      },
+    },
+  };
 });
