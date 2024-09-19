68c61172a516817babbd6c044f8e6d88bc8d545c
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2024-09-18 21:04:15 -0400

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import http from "https";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_BACK_END,
-          changeOrigin: true,
-          secure: false,
-          agent: new http.Agent(),
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
